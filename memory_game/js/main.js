0f64dbfa6879b18b00e81f19e0ec20b959928be0
David Campbell <dcampbell092@gmail.com>
2019-11-04 11:54:59 +0000

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,21 +2,23 @@ console.log("Up and running!");
 
 var cards = ["Queen", "Queen", "King", "King"];
 
-var cardOne = cards[0]; {
-console.log("User flipped " + cards[0]);
-}
-
-var cardTwo = cards[1];	{
-console.log("User flipped " + cards[1]);
-}
-
 const cardsInPlay = [];
-	cardsInPlay.push(cardOne);
-	cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2) {
-	} if (cardOne === cardTwo) {
-		alert("Match found!");
-	} else {
-		alert("Try Again!");
+function checkForMatch () {
+		if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		  console.log("You found a match!");
+		} else {
+		  console.log("Sorry, try again.");
+		}
 	}
+}
+
+function flipCard(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
